154a8658e7f04081e9d889204bb1fc1c9d7eac30
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-02-21 20:43:06 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -3,18 +3,20 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const btnOpen = document.createElement("button");
-btnOpen.innerHTML = "Open File";
-btnOpen.addEventListener("click", function () {
-  const inpFile = document.createElement("input");
-  inpFile.type = "file";
-  inpFile.addEventListener("input", function () {
-    const script = document.createElement("script");
-    script.src = URL.createObjectURL(inpFile.files[0]);
-    inpFile.remove();
-    document.body.appendChild(script);
+window.addEventListener("load", function () {
+  const btnOpen = document.createElement("button");
+  btnOpen.innerHTML = "Open File";
+  btnOpen.addEventListener("click", function () {
+    const inpFile = document.createElement("input");
+    inpFile.type = "file";
+    inpFile.addEventListener("input", function () {
+      const script = document.createElement("script");
+      script.src = URL.createObjectURL(inpFile.files[0]);
+      inpFile.remove();
+      document.body.appendChild(script);
+    });
+    document.body.appendChild(inpFile);
+    inpFile.click();
   });
-  document.body.appendChild(inpFile);
-  inpFile.click();
+  document.body.appendChild(btnOpen);
 });
-document.body.appendChild(btnOpen);
